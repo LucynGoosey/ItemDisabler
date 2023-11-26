fd15cdc5fca5968c9931401acfeb0781c3fba56c
Lucyndifer <didgeridev@gmail.com>
2023-11-25 23:08:31 -0500

    Added PearlListener.java

---
+++ src/main/java/me/lucyn/itemdisabler/listeners/PearlListener.java
@@ -0,0 +1,12 @@
+package me.lucyn.itemdisabler.listeners;
+
+import me.lucyn.itemdisabler.ItemDisabler;
+import org.bukkit.event.Listener;
+
+public class PearlListener implements Listener {
+    private ItemDisabler plugin;
+    public PearlListener(ItemDisabler plugin) {
+        this.plugin = plugin;
+    }
+
+}
