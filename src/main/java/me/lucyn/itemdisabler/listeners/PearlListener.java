65bd3b0eee8b76a5104a4de034d1ba01f80c8fad
Lucyndifer <didgeridev@gmail.com>
2023-11-25 23:51:22 -0500

    Added PearlListener.java

--- src/main/java/me/lucyn/itemdisabler/listeners/PearlListener.java
+++ src/main/java/me/lucyn/itemdisabler/listeners/PearlListener.java
@@ -1,10 +1,13 @@
 package me.lucyn.itemdisabler.listeners;
 
 import me.lucyn.itemdisabler.ItemDisabler;
-import org.bukkit.Material;
+
+import org.bukkit.entity.EnderPearl;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+
 
 
 public class PearlListener implements Listener {
@@ -14,8 +17,8 @@ public class PearlListener implements Listener {
     }
 
     @EventHandler
-    public void onPearlThrown(PlayerItemConsumeEvent event) {
-        if(plugin.getConfig().getBoolean("pearl") && event.getItem().getType() == Material.ENDER_PEARL) {
+    public void onPearlThrown(ProjectileLaunchEvent event) {
+        if(plugin.getConfig().getBoolean("pearl") && event.getEntity() instanceof EnderPearl && event.getEntity().getShooter() instanceof Player) {
             event.setCancelled(true);
 
         }
